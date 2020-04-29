b6ecc17056320b2f5de2fd82b88eb73e8c443677
sergey_dmitrenko <serge2352008@gmail.com>
2020-04-29 22:43:10 +0300

    fixed variable names

--- game2.py
+++ game2.py
@@ -1,38 +1,38 @@
 from random import shuffle
 
-def diler(i):
+def dealer(player_score):
     count = 0
-    current = koloda.pop()
+    current = deck.pop()
     count += current
-    current = koloda.pop()
+    current = deck.pop()
     count += current
-    while count < 21 and count < i:
-        current = koloda.pop()
+
+    while count < 21 and count < player_score:
+        current = deck.pop()
         count += current
     print("Счёт дилера: ", count)
     return count
 
 def game():
     count = 0
-    current = koloda.pop()
+    current = deck.pop()
     count += current
-
-    current = koloda.pop()
+    current = deck.pop()
     count += current
-
     print("Ваш счёт: ", count)
+
     while count < 21:
         k = input("Тянуть карту? 0 - да, 1 - нет ")
         if k == '0':
-            current = koloda.pop()
+            current = deck.pop()
             count += current
             print("Ваш счёт: ", count)
     
         elif k == '1':
-            count_diler = diler(count)
-            if count > count_diler or count_diler > 21:
+            count_dealer = dealer(count)
+            if count > count_dealer or count_dealer > 21:
                 print("Поздравляем! Вы выиграли!")
-            elif count == count_diler:
+            elif count == count_dealer:
                 print("Ничья!")
             else:
                 print("Вы проиграли!")
@@ -42,19 +42,19 @@ def game():
             continue
     else:
         if count < 22:
-            count_diler = diler(count)
-            if count > count_diler or count_diler > 21:
+            count_dealer = dealer(count)
+            if count > count_dealer or count_dealer > 21:
                 print("Поздравляем! Вы выиграли!")
-            elif count == count_diler:
+            elif count == count_dealer:
                 print("Ничья!")
             else:
                 print("Вы проиграли!")
         else:
             print("Вы проиграли!")
 
-v = '0'
-while v == '0':
-    koloda = [6,7,8,9,10,2,3,4,11] * 4
-    shuffle(koloda)
+play = '0'
+while play = '0':
+    deck = [6,7,8,9,10,2,3,4,11] * 4
+    shuffle(deck)
     game()
-    v = input("Новая игра? 0 - да, 1 - нет ")
+    play = input("Новая игра? 0 - да, 1 - нет ")
